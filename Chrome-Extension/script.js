a462c64a4bd34339a4f98db544007081793232ec
fitzwolf <danfelker@gmail.com>
2022-12-05 21:36:24 -0700

    commented code extensively and brief lat/long

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -1,7 +1,28 @@
+// The general idea of this whole app is to take a collection of 
+// reviews - in this sample we are using Yelp's academic dataset -
+// and from those reviews then all the users to ask for more detailed
+// information such as restaurants that have a cozy environment.
+// to accomplish this there are several steps. The first is to turn
+// the dataset into an index using dataset.py then start up the
+// app.py which will make a local webserver that defaults
+// to port 8080. This port will then be communicated to by either
+// either directly accessing index.html or through the chrome-extension
+// titled RestaurantFinder.
+// index.html builds a table and has a search textfield that triggers
+// in thie file: sendData() which in turn uses fetchData to send the
+// query and then take the returned results and parse out the
+// restaurant name, address, city and stars and display them
+// into the table in index.html. By default the code will also
+// get the user's location and use that as part of the search
+
+
+// lat and long are used to store the user's latitude and longitude
 var lat = 0.0;
 var long = 0.0;
+// this function is triggered when the user clicks Go on the index.html
 document.getElementById("choice").onclick = function sendData () {
-    // (A) GET FORM DATA
+    // (A) GET FORM DATA - this needs to be modified if you change the
+    // default url
     link = "http://127.0.0.1:8080/find?q=" + document.getElementById("Restaurant-Search").value + "&latitude=" + lat + "&longitude=" + long
     console.log(link);
     fetchData(link)
@@ -10,6 +31,7 @@ document.getElementById("choice").onclick = function sendData () {
     return false;
   }
 
+  // this script gets the user location (lat and long)
 var x = document.getElementById("loc");
 function getLocation() {
     if (navigator.geolocation) {
@@ -19,12 +41,16 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
+    x.innerHTML = "This search is using your default location of Latitude: " + position.coords.latitude + 
+    " and Longitude: " + position.coords.longitude + 
+    ".<br> But because the academic dataset is small" +
+    " some results might be further away than expected.";
     lat = position.coords.latitude;
     long = position.coords.longitude; 
 }
 
+// this function retrieves the data from the search and appends it to the table
+// for easy viewing.
 async function fetchData(link) {
     const res=await fetch (link, {
       method: 'GET',
@@ -44,6 +70,7 @@ async function fetchData(link) {
       var row = `<tr>
         <td>${record[i].name}</td>
         <td>${record[i].address}</td>
+        <td>${record[i].city}</td>
         <td>${record[i].stars}</td>
       </tr>
       `
